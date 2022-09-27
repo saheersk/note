9c8d7e76079b3dceb632d414c18619bd7092bb3d
Muhammed saheerkhan <muhammedsaheerkhan@Saheers-Air.Dlink>
2022-09-27 22:05:14 +0530

    error-corrected

--- api/v1/actions/urls.py
+++ api/v1/actions/urls.py
@@ -3,7 +3,7 @@ from api.v1.actions import views
 
 urlpatterns = [
     path('', views.actions),
-    path('<str:pk>/', views.actions_read),
+    path('view/<str:pk>/', views.actions_read),
     path('create/', views.actions_post),
     path('update/<str:pk>', views.actions_update),
     path('delete/<str:pk>', views.actions_delete),
